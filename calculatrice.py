713571b39fd7650c6c84bf267dd41dba664c0295
FUGERAY <hugo.fugeray@gmail.com>
2021-04-27 14:32:58 +0200

    modif

--- calculatrice.py
+++ calculatrice.py
@@ -1,47 +1,49 @@
-def askUser(sentence = "Saisir un chiffre"): # do_something
+def ask_user(sentence = "Saisir un chiffre"):
     choice = input(f"""{sentence}\n>""")
     return choice
 
-def Addition(number): # do_something
+def addition(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
             list_numbers.append(int(number))
-        number = ask_user("Saisir un ciffre à additionner ou clicker sur '=' ")
-    result = list_numbers # do_something
+        number = ask_user("Saisir un chiffre à additionner ou saisir '=' ")
+    result = sum(list_numbers)
     return result
 
-def multplication(number):
+def multiplication(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
-            list_numbers.append(number) # do_something
+            list_numbers.append(int(number))
         number = ask_user("Saisir un ciffre à multiplier ou clicker sur '=' ")
     for list_number, index in zip(list_numbers,list(range(len(list_numbers)))): # refactoriser
-        if index == '0': # do_something
+        if index == 0:
             result = list_number
         else:
-            result = result / list_number # do_something
+            result = result * list_number
     return result
 
 def division(number):
     list_numbers = []
     while number.isdigit():
         if number.isdigit():
-            list_numbers.append(number) # do_something
+            list_numbers.append(int(number))
         number = ask_user("Saisir un ciffre à multiplier ou clicker sur '=' ")
     for list_number, index in zip(list_numbers,list(range(len(list_numbers)))): # refactoriser
         if index == 0:
             result = list_number
+        elif list_number!=0:
+            result = result / list_number
         else:
-            result = result + list_number # do_something
+            print('Tu divises par 0 toi ?!')
     return result
 
 def soustraction(number):
     list_numbers = []
-    while number.isdigit: # do_something
+    while number.isdigit():
         if number.isdigit():
-            list_numbers.append(number) # do_something
+            list_numbers.append(int(number))
         number = ask_user("Saisir un ciffre à additionner ou clicker sur '=' ")
     i = 0
     for list_number in list_numbers:
@@ -52,27 +54,35 @@ def soustraction(number):
         i = i + 1
     return result
 
+
 def display_interface():
-    choice = ask_user("""
-    Tu veux :
-    1. Additionner Tape 1
-    2. Soustraire Tape 2
-    3. Multiplier Tape 3
-    4. Diviser Tape 4""")
-    while choice.isdigit():
-        choice = choice # do_something
+    i=True
+    while i :
+        choice = ask_user("""
+        Tu veux :
+        1. Additionner Tape 1
+        2. Soustraire Tape 2
+        3. Multiplier Tape 3
+        4. Diviser Tape 4
+        5. Quitter""")
+
+        choice = int(choice)
         if choice == 1:
-            choice = ask_user("Saisir un ciffre à ADDITIONNER ou clicker sur '=' ")
+            choice = ask_user("Saisir un chiffre à ADDITIONNER ou clicker sur '=' ")
             result = addition(choice)
         elif choice == 2:
             choice = ask_user("Saisir un ciffre à SOUSTRAIRE ou clicker sur '=' ")
             result = soustraction(choice)
         elif choice == 3:
             choice = ask_user("Saisir un ciffre à MULTIPLIER ou clicker sur '=' ")
-            result = multplication(choice)
+            result = multiplication(choice)
         elif choice == 4:
             choice = ask_user("Saisir un ciffre à MULTIPLIER ou clicker sur '=' ")
             result = division(choice)
-        return print(f"Le resultat est ==> {result}")
+        elif choice==5:
+          print("Aurevoir")
+          break
+        print(f"Le resultat est ==> {result}")
+
 
 
