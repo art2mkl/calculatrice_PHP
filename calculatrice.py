5e02afea3af5e86ddf27ed26e2efd2c389544950
BAGUIDOU <baguidou@MacBook-Air-de-BAGUIDOU.local>
2021-04-27 16:43:21 +0200

    fonction = pour lancer le calcul

--- calculatrice.py
+++ calculatrice.py
@@ -1,3 +1,4 @@
+
 def ask_user(sentence = "Saisir un chiffre"):
     choice = input(f"""{sentence}\n>""")
     return choice
@@ -8,8 +9,14 @@ def addition(number):
         if number.isdigit():
             list_numbers.append(int(number))
         number = ask_user("Saisir un chiffre à ADDITIONNER ou saisir '=' ")
+        
+        while not number.isdigit() and number != "=":
+            print("c'est pas ce qu'on t'as demandé")
+            number = ask_user("Saisir un chiffre à ADDITIONNER ou saisir '=' ")
+
     result = sum(list_numbers)
     return result
+
 
 def multplication(number):
     list_numbers = []
@@ -17,6 +24,11 @@ def multplication(number):
         if number.isdigit():
             list_numbers.append(int(number))
         number = ask_user("Saisir un chiffre à MULTIPLIER ou saisir '=' ")
+
+        while not number.isdigit() and number != "=":
+            print("c'est pas ce qu'on t'as demandé")
+            number = ask_user("Saisir un chiffre à MULTIPLIER ou saisir '=' ")
+
     for index, list_number in enumerate(list_numbers):
         if index == 0:
             result = list_number
@@ -30,6 +42,11 @@ def division(number):
         if number.isdigit():
             list_numbers.append(int(number))
         number = ask_user("Saisir un chiffre à DIVISER ou saisir '=' ")
+
+        while not number.isdigit() and number != "=":
+            print("c'est pas ce qu'on t'as demandé")
+            number = ask_user("Saisir un chiffre à DIVISER ou saisir '=' ")
+
     for index, list_number in enumerate(list_numbers):
         if index == 0:
             result = list_number
@@ -45,6 +62,11 @@ def soustraction(number):
         if number.isdigit():
             list_numbers.append(int(number))
         number = ask_user("Saisir un chiffre à SOUSTRAIRE ou saisir '=' ")
+
+        while not number.isdigit() and number != "=":
+            print("c'est pas ce qu'on t'as demandé")
+            number = ask_user("Saisir un chiffre à SOUSTRAIRE ou saisir '=' ")
+
     i = 0
     for list_number in list_numbers:
         if i == 0:
@@ -64,26 +86,28 @@ def display_interface():
         2. Soustraire Tape 2
         3. Multiplier Tape 3
         4. Diviser Tape 4
-        5. Quitter""")
+        5. Quitter
+        """)
 
-
-        choice = int(choice)
-        if choice == 1:
-            choice = ask_user("Saisir un chiffre à ADDITIONNER ou saisir '=' ")
-            result = addition(choice)
-        elif choice == 2:
-            choice = ask_user("Saisir un chiffre à SOUSTRAIRE ou saisir '=' ")
-            result = soustraction(choice)
-        elif choice == 3:
-            choice = ask_user("Saisir un chiffre à MULTIPLIER ou saisir '=' ")
-            result = multplication(choice)
-        elif choice == 4:
-            choice = ask_user("Saisir un chiffre à DIVISER ou saisir '=' ")
-            result = division(choice)
-        elif choice == 5:
-            print("Au revoir")
-            break
-
-        print(f"Le resultat est ==> {result}")
+        try:
+            choice = int(choice)
+            if choice == 1:
+                choice = ask_user("Saisir un chiffre à ADDITIONNER ou saisir '=' ")
+                result = addition(choice)
+            elif choice == 2:
+                choice = ask_user("Saisir un chiffre à SOUSTRAIRE ou saisir '=' ")
+                result = soustraction(choice)
+            elif choice == 3:
+                choice = ask_user("Saisir un chiffre à MULTIPLIER ou saisir '=' ")
+                result = multplication(choice)
+            elif choice == 4:
+                choice = ask_user("Saisir un chiffre à DIVISER ou saisir '=' ")
+                result = division(choice)
+            elif choice == 5:
+                print("Au revoir")
+                break
+            print(f"Le resultat est ==> {result}")
+        except:
+            print("c'est pas un chiffre")
         
 
